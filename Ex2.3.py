94c67ae6d8417227e5a7e965b0af67bcb06e467d
KenanaTurabi <keananaturabi@gmail.com>
2022-07-14 00:07:21 +0300

    first commit

---
+++ Ex2.3.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+years=90-int(age)
+days=365*years
+weeks=years*52
+months=12*years
+print(f"you have {days} days, {weeks} weeks and {months} months left.")
